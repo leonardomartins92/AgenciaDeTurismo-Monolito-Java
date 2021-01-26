ebd7cc7fe8c654d6b2e6410a59b07a8ac16e116d
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-01-26 15:05:26 -0300

    Novas pastas

---
+++ backend/src/Pessoa.java
@@ -0,0 +1,7 @@
+public abstract class Pessoa {
+    protected String name;
+    protected String cpf;
+    protected String logradouro;
+    protected String numero;
+    protected String complemento;
+}
