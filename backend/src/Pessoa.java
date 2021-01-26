7b6a56fff000613dd589e779d3cc49b1b34113c5
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-01-26 15:27:58 -0300

    Editando Classes

--- backend/src/Pessoa.java
+++ backend/src/Pessoa.java
@@ -1,5 +1,7 @@
 public abstract class Pessoa {
     protected String name;
+    protected String telefone;
+    protected String email;
     protected String cpf;
     protected String logradouro;
     protected String numero;
