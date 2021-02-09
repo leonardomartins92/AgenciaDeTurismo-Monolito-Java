6951f7b8a3f755652b6b65282298db2e141d83dc
Fabrício Lima <45342038+Fabriciooml@users.noreply.github.com>
2021-02-09 14:32:29 -0300

    bug fecharConexao resolvido

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
-/**
- *
- * @author fabri
- */
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.SQLException;
+
 public class DAO {
-    
+    public void fecharConexao(Connection conexao, Statement comando)
+            throws SQLException {
+        if (comando != null){
+            comando.close();
+        }
+        if (conexao != null){
+            conexao.close();
+        }
+    }
 }
