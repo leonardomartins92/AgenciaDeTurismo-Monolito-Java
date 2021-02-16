0bc101e2b2f3c87cd4eeca1710339cf6f07dc0d7
Fabrício Lima <45342038+Fabriciooml@users.noreply.github.com>
2021-02-16 15:25:15 -0300

    Teste Cliente e Empresa dps do novo banco

--- src/java/controller/TestCliente.java
+++ src/java/controller/TestCliente.java
@@ -1,4 +1,4 @@
-package exception;
+package controller;
 
 import model.Cliente;
 import dao.ClienteDAO;
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class TestCliente {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
        Cliente cliente = new Cliente("Mario","123","leo@","1012","R ABC", "100","nada","MG","JF","a");
-       //ClienteDAO.getInstancia().gravar(cliente);
-       //System.out.println(ClienteDAO.getInstancia().obterClientes());
-       //System.out.println(ClienteDAO.getInstancia().obterCliente("1012"));
-       //ClienteDAO.getInstancia().deletarCliente("1012");
+       ClienteDAO.getInstancia().gravar(cliente);
+       System.out.println(ClienteDAO.getInstancia().obterClientes());
+       System.out.println(ClienteDAO.getInstancia().obterCliente("1012"));
+       ClienteDAO.getInstancia().deletarCliente("1012");
     }
 }
